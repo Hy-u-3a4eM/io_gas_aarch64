9f944b130b674313dfadf2d6c874f173ea2d5855
Hy-u-3a4eM <za4emsu@gmail.com>
2022-07-07 00:33:15 +0300

    Slightly optimized the io file.

--- io/io.s
+++ io/io.s
@@ -3,7 +3,7 @@
 	.p2align	2
 	.type	_start,@function
 _start:
-	mov x0, #0
+	mov x0, xzr
 	ldr x1, =io
 	ldr x2, =io
 	mov w8, #63
@@ -15,7 +15,7 @@ _start:
 	mov w8, #64
 	svc #0
 
-	mov x0, #0
+	mov x0, xzr
 	mov w8, #93
 	svc #0
 .Lfunc_end0:
